4eaca3e23d46be985680bd0bc772a73761ef7bdf
thetanaz <nasko235679@gmail.com>
2024-07-04 01:51:38 +0300

    origin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Embla } from "@/components/embla";
 import SwipeCarousel from "@/components/swipe-carousel";
+import { unstable_noStore } from "next/cache";
 
 export default function Home() {
+  unstable_noStore();
   return (
     <main>
       <div className="flex items-center mx-auto justify-center mt-10 w-[1200px] h-[600px] ">
