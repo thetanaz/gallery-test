c0727caf32ecadb55f6f00e7199e3e2fbdc93660
thetanaz <nasko235679@gmail.com>
2024-07-04 12:41:29 +0300

    origin

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SwipeCarousel from "@/components/swipe-carousel";
 export default function Home() {
   return (
     <main>
-      <div className="flex items-start mx-2 justify-center mt-10 w-[1200px] h-[600px] ">
+      <div className="flex items-center mx-auto justify-center mt-10 w-[350px] h-[1000px] sm:w-[1200px] sm:h-[600px] ">
         <Embla />
       </div>
     </main>
